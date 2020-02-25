ac7c6f427229c0ae5938fa943002934d09b5acb3
rpkpmara <55075965+rpkpmara@users.noreply.github.com>
2020-02-25 14:40:21 +0900

    Add files via upload

---
+++ 다오봇.py
@@ -0,0 +1,36 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("준비완료")
+    game = discord.Game("@도움말로 확인")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("@안녕"):
+        await message.channel.send("하이")
+    if message.content.startswith("@도움말"):
+        await message.channel.send("배불룩배찌, 제작, 안녕, 오늘의 날씨, play (현재 개발중)")
+    if message.content.startswith("@제작"):
+        await message.channel.send("배찌★#8008")
+    if message.content.startswith("@오늘의 날씨"):
+        await message.channel.send("https://weather.naver.com/")
+    if message.content.startswith("@배불룩배찌"):
+        await message.channel.send("https://www.youtube.com/channel/UCDuwrfmdvP9e4pcl-Ew1S0w")
+
+
+
+
+
+
+
+
+
+
+client.run("NjgwNjY4NzM1OTA4MDg1Nzgy.XlM0-Q.aLr8Ttu_XVcn7z74ocP4uVRU98g")
